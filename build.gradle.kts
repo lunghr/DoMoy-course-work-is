3ddf1b3595e560dddf517a261acb12e6b15091ad
lunghr <myilanka8@gmail.com>
2026-01-17 11:45:44 +0300

    build: removed excessive dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     kotlin("plugin.jpa") version "2.1.10"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.github.ben-manes.versions") version "0.51.0" // Обновлен плагин версий
-    id("com.autonomousapps.dependency-analysis") version "2.0.0" // Обновлен плагин анализа
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.autonomousapps.dependency-analysis") version "2.0.0"
 }
 
 group = "com.example"
@@ -22,42 +22,19 @@ repositories {
 }
 
 dependencies {
-    // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-
-    // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    // Database & Migration
     runtimeOnly("org.postgresql:postgresql")
-    implementation("org.flywaydb:flyway-core") // Версия управляется Spring Boot
-
-    // Utils & Third Party
     implementation("io.jsonwebtoken:jjwt:0.12.6")
     implementation("io.minio:minio:8.5.17")
     implementation("org.apache.poi:poi-ooxml:5.4.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
-
-    // Documentation (Swagger/OpenAPI)
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
-
-    // Development
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-
-    // Testing
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.springframework:spring-webflux")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
